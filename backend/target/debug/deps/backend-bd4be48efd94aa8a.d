81061495ad7ce5584a02235723124198216534f6
Davor Petrovikj <92484678+dxvx2@users.noreply.github.com>
2024-03-04 01:12:35 -0500

    new backend unit tests
    at this point everything but github oauth works,

---
+++ backend/target/debug/deps/backend-bd4be48efd94aa8a.d
@@ -0,0 +1,9 @@
+/mnt/c/Users/davor/OneDrive - NJIT/Desktop/github/Binary-Brains/backend/target/debug/deps/backend-bd4be48efd94aa8a: src/main.rs src/models.rs src/handlers.rs src/db.rs src/auth.rs
+
+/mnt/c/Users/davor/OneDrive - NJIT/Desktop/github/Binary-Brains/backend/target/debug/deps/backend-bd4be48efd94aa8a.d: src/main.rs src/models.rs src/handlers.rs src/db.rs src/auth.rs
+
+src/main.rs:
+src/models.rs:
+src/handlers.rs:
+src/db.rs:
+src/auth.rs:
---
+++ backend/target/debug/deps/uuid-257aa4d1160ed80b.d
@@ -0,0 +1,7 @@
+/mnt/c/Users/davor/OneDrive - NJIT/Desktop/github/Binary-Brains/backend/target/debug/deps/libuuid-257aa4d1160ed80b.rmeta: /home/dp788/.cargo/registry/src/index.crates.io-6f17d22bba15001f/uuid-0.4.0/src/lib.rs
+
+/mnt/c/Users/davor/OneDrive - NJIT/Desktop/github/Binary-Brains/backend/target/debug/deps/libuuid-257aa4d1160ed80b.rlib: /home/dp788/.cargo/registry/src/index.crates.io-6f17d22bba15001f/uuid-0.4.0/src/lib.rs
+
+/mnt/c/Users/davor/OneDrive - NJIT/Desktop/github/Binary-Brains/backend/target/debug/deps/uuid-257aa4d1160ed80b.d: /home/dp788/.cargo/registry/src/index.crates.io-6f17d22bba15001f/uuid-0.4.0/src/lib.rs
+
+/home/dp788/.cargo/registry/src/index.crates.io-6f17d22bba15001f/uuid-0.4.0/src/lib.rs:
