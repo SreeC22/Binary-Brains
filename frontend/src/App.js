d0025e4bc36c2c910dd6883ea8cffa1700191b9a
Sreekaree Chityala <sc354@njit.edu>
2024-03-04 20:10:31 -0500

    Dave and Sree's merged Code
    We merged feedback and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,22 +11,21 @@ import Navbar from './Components/NavBar';
 import { AuthProvider } from './Components/AuthContext'; // Adjust path as necessary
 import HomePage from './Pages/HomePage';
 import OAuthCallbackHandler from './Components/OAuthCallbackHandler'; // Import the handler
-// Other imports...
 import FeedbackPage from './Pages/FeedbackPage/FeedbackPage';
 
 function App() {
   return (
     <Router>
-            <AuthProvider> {/* Ensure this wraps Navbar and other components */}
+      <AuthProvider> {/* Ensure this wraps Navbar and other components */}
 
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/oauth_callback" element={<OAuthCallbackHandler />} /> {/* Add this line */}
-        <Route path="/" element={<HomePage />} /> {/* Define the route to HomePage */}
-        <Route path="/feedback" element={<FeedbackPage />} />
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/oauth_callback" element={<OAuthCallbackHandler />} /> {/* Add this line */}
+          <Route path="/" element={<HomePage />} /> {/* Define the route to HomePage */}
+          <Route path="/feedback" element={<FeedbackPage />} />
 
-      </Routes>
+        </Routes>
       </AuthProvider>
 
     </Router>
