2a9c2f757e6b3fa484f1199f3dfc6bdacbefc490
Erantha Arachchi <era3@njit.edu>
2024-03-05 05:48:04 -0500

    added a better alert box. made it so that when you hover over the language icons it zooms in a little.

--- frontend/src/Pages/TranslateCode.js
+++ frontend/src/Pages/TranslateCode.js
@@ -1,5 +1,5 @@
 
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, CloseButton, Button as CustomButton, Flex, FormLabel, HStack, Menu, MenuButton, MenuItem, MenuList, Text, VStack } from "@chakra-ui/react";
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, CloseButton, Button as CustomButton, Flex, FormLabel, HStack, Menu, MenuButton, MenuItem, Slide, MenuList, Text, VStack, useColorMode, ChakraProvider } from "@chakra-ui/react";
 import ace from 'ace-builds/src-noconflict/ace'; // this isnt used but it needs to be here for it to work. idk why.
 import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/mode-csharp';
@@ -13,80 +13,13 @@ import 'ace-builds/src-noconflict/mode-swift';
 import 'ace-builds/src-noconflict/mode-typescript';
 import 'ace-builds/src-noconflict/theme-github';
 import 'ace-builds/src-noconflict/theme-monokai';
-import { ChakraProvider } from '@chakra-ui/react';
 import { CplusplusOriginal, CsharpOriginal, JavaOriginal, MatlabOriginal, PerlOriginal, PythonOriginal, RubyOriginal, RustOriginal, SwiftOriginal, TypescriptOriginal } from 'devicons-react';
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import AceEditor from 'react-ace';
+import { motion } from "framer-motion"; // Import motion from Framer Motion
 import { BiSolidDownArrowAlt } from "react-icons/bi";
 import { FaCode, FaCog, FaCube, FaPaste } from 'react-icons/fa';
 import { SiConvertio } from "react-icons/si";
-
-import { useColorMode } from '@chakra-ui/react';
-
-
-
-
-
-
-
-
-const CustomAlert = ({ status, message, onClose }) => {
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    if (!visible) {
-      const timer = setTimeout(() => {
-        onClose();
-      }, 500); // Adjust the timeout to match the transition duration
-      return () => clearTimeout(timer);
-    }
-  }, [visible, onClose]);
-
-  const handleClose = () => {
-    setVisible(false);
-  };
-
-  return (
-    <Alert
-      status={'error'}
-      variant="subtle"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      textAlign="center"
-      height="200px"
-      position="fixed" /* Fixed position */
-      top="50%" /* Center vertically */
-      left="50%" /* Center horizontally */
-      transform="translate(-50%, -50%)" /* Center using transform */
-      zIndex="9999" /* Ensure it's above other content */
-      maxWidth="20%" /* Limit width to 80% of viewport */
-      background="#FED7D7" /* Background color */
-      boxShadow="16" /* Add shadow */
-      borderRadius="13" /* Add border radius */
-      padding="24px" /* Add padding */
-      opacity={visible ? 1 : 0} /* Conditionally set opacity */
-      fontFamily="Lato"
-    >
-      <AlertIcon boxSize="40px" mr={0} color="#C53030"/>
-      <AlertTitle mt={4} mb={1} fontSize="28"  fontWeight="bold">
-        {status === 'success' ? 'Success' : 'Error:'}
-      </AlertTitle>
-      <AlertDescription maxWidth="sm" fontSize="16"  >
-        {message}
-      </AlertDescription>
-      <CloseButton
-        position="absolute"
-        right="8px"
-        top="8px"
-        onClick={handleClose}
-        _focus={{ boxShadow: "none" }}
-        _active={{ boxShadow: "none" }}
-      />
-    </Alert>
-  );
-};
-
 const languages = [
   { label: "Python", value: "python", icon: <PythonOriginal /> },
   { label: "Java", value: "java", icon: <JavaOriginal /> },
@@ -114,7 +47,9 @@ const TranslateCode = () => {
   const [sourceLanguage, setSourceLanguage] = useState("");
   const [error, setError] = useState("");
   
-
+  const handleClose = () => {
+    setError(""); // Clear the error message
+  };
   const handleConvert = () => {
     if (!sourceLanguage || !targetLanguage) {
       setError("Both source and target languages are required");
@@ -144,9 +79,7 @@ const TranslateCode = () => {
       setInputCode(text);
     })
   };
-  const handleCloseAlert = () => {
-    setError("");
-  };
+
 
   const HeadingSteps = () => (
     <Box paddingY={4} ml="auto" style={{ backgroundColor }}>
@@ -175,18 +108,18 @@ const TranslateCode = () => {
   return (
     <>
        <ChakraProvider>
-    
+       
       <HeadingSteps />
+      
       <VStack spacing={4} align="stretch" style={{ backgroundColor, minHeight: "120vh" }}>
-        {error && (
-          <CustomAlert status="error" message={error} onClose={handleCloseAlert}  />
-        )}
         <Box width="47.75%" bg="#DFD3D33B" p={4} borderRadius="16" textAlign="center" display="flex" alignItems="center" justifyContent="center">
   <Text fontFamily="Roboto" marginBottom={0} fontSize={24}>Drop your code here!</Text>
   <Box ml={2}>
     <BiSolidDownArrowAlt size={32} />
   </Box>
+
 </Box>
+
         <Flex justifyContent="space-between">
           <Box position="relative">
             <Menu>
@@ -306,6 +239,31 @@ const TranslateCode = () => {
             />
           </Box>
         </Box>
+        <div style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 9999, pointerEvents: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Slide in={Boolean(error)} direction="bottom">
+            {error && (
+              <Alert 
+                status="error" 
+                variant="solid" 
+                flexDirection="column" 
+                alignItems="center" 
+                justifyContent="center" 
+                textAlign="center" 
+                height="200px"
+                style={{pointerEvents: 'auto' }} // Enable pointer events for the alert
+              >
+                <AlertIcon boxSize="40px" mr={0} />
+                <AlertTitle mt={4} mb={1} fontSize="lg">
+                  Error:
+                </AlertTitle>
+                <AlertDescription maxWidth="sm">
+                  {error}
+                </AlertDescription>
+                <CloseButton position="absolute" right="8px" top="8px" onClick={handleClose} />
+              </Alert>
+            )}
+        </Slide>
+      </div>
         <CustomButton 
           backgroundColor="black"
           color="white" 
@@ -336,18 +294,20 @@ const TranslateCode = () => {
         </Text>
         
         <Box>
-          <Text fontSize="21">Try our Code Generators in other languages:</Text>
-          <Flex>
-            {languages.map(lang => (
-              <Box key={lang.value} bg="white" p={4} borderRadius="md" textAlign="center" mr={4} width="100px" height="80px">
+        <Text fontSize="21">Try our Code Generators in other languages:</Text>
+        <Flex>
+          {languages.map(lang => (
+            <motion.div key={lang.value} whileHover={{ scale: 1.25 }}>
+              <Box bg="white" p={4} borderRadius="md" textAlign="center" mr={4} width="100px" height="80px">
                 <VStack spacing={0}>
                   {React.cloneElement(lang.icon, { size: 42 })} {/* Adjust size of the icon */}
                   <Text fontSize="16" fontFamily="Roboto" color={textColor}> {lang.label}</Text> {/* Adjust font size of the label */}
                 </VStack>
               </Box>
-            ))}
-          </Flex>
-        </Box>
+            </motion.div>
+          ))}
+        </Flex>
+      </Box>
         <Box mt="auto" py={4} fontFamily="Roboto" textAlign="center" borderTop="1px solid" borderTopColor="gray.300">
           © 2024 Binary Brains. All rights reserved.
         </Box>
