f72b93929466d54568ebeb58b3bf21e88c1036f9
Sreekaree Chityala <sc354@njit.edu>
2024-03-24 14:50:47 -0400

    All the tests passed. Final Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,13 @@ import { AuthProvider } from './Components/AuthContext'; // Ensure this path is 
 import './App.css';
 import NavBar from './Components/NavBar';
 import HomePage from './Pages/HomePage';
-import CodeConvertPage from './Pages/CodeConvertPage';
 import FeedbackPage from './Pages/FeedbackPage/FeedbackPage';
 import LoginPage from './Pages/LoginPage';
 import DocumentationPage from './Pages/DocumentationPage';
-import TutorialPage from './Pages/TutorialPage';
-import FAQsPage from './Pages/FAQsPage';
+import FAQsPage from './Pages/FaqsPage/FAQsPage';
 import AboutUS from './Pages/AboutUsPage';
 import Contactus from './Pages/ContactUs';
 import TranslateCode from './Pages/TranslateCode';
-import HelpPage from './Pages/HelpPage';
 import { ColorModeProvider } from "./Pages/ColorModeContext";
 
 function App() {
@@ -26,16 +23,13 @@ function App() {
           <NavBar /> {/* This will be displayed on every page */}
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/code-conversion" element={<CodeConvertPage />} />
             <Route path="/feedback" element={<FeedbackPage />} />
             <Route path="/documentation" element={<DocumentationPage />} />
-            <Route path="/tutorial" element={<TutorialPage />} />
             <Route path="/faqs" element={<FAQsPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/AboutUs" element={<AboutUS />} />
             <Route path="/ContactUs" element={<Contactus />} />
             <Route path="/translate" element={<TranslateCode />} />
-            <Route path="/help" element={<HelpPage />} />
           </Routes>
         </AuthProvider>
 
