7bac2c3cd5f7bcf58f57cac77b5ccd7845ffa8ea
Sreekaree Chityala <sc354@njit.edu>
2024-03-26 15:20:49 -0400

    History Button Works and Displays everything

---
+++ backend/target/debug/deps/auth_handlers-d983e5f2282dc182.d
@@ -0,0 +1,5 @@
+/Users/sreekareechityala/Desktop/NJIT/2023-2024/Spring 2024/CS490/Binary Brains/Binary-Brains/backend/target/debug/deps/auth_handlers-d983e5f2282dc182: tests/auth_handlers.rs
+
+/Users/sreekareechityala/Desktop/NJIT/2023-2024/Spring 2024/CS490/Binary Brains/Binary-Brains/backend/target/debug/deps/auth_handlers-d983e5f2282dc182.d: tests/auth_handlers.rs
+
+tests/auth_handlers.rs:
---
+++ backend/target/debug/deps/simple_asn1-61cf40f76be7c304.d
@@ -0,0 +1,7 @@
+/Users/sreekareechityala/Desktop/NJIT/2023-2024/Spring 2024/CS490/Binary Brains/Binary-Brains/backend/target/debug/deps/libsimple_asn1-61cf40f76be7c304.rmeta: /Users/sreekareechityala/.cargo/registry/src/index.crates.io-6f17d22bba15001f/simple_asn1-0.4.1/src/lib.rs
+
+/Users/sreekareechityala/Desktop/NJIT/2023-2024/Spring 2024/CS490/Binary Brains/Binary-Brains/backend/target/debug/deps/libsimple_asn1-61cf40f76be7c304.rlib: /Users/sreekareechityala/.cargo/registry/src/index.crates.io-6f17d22bba15001f/simple_asn1-0.4.1/src/lib.rs
+
+/Users/sreekareechityala/Desktop/NJIT/2023-2024/Spring 2024/CS490/Binary Brains/Binary-Brains/backend/target/debug/deps/simple_asn1-61cf40f76be7c304.d: /Users/sreekareechityala/.cargo/registry/src/index.crates.io-6f17d22bba15001f/simple_asn1-0.4.1/src/lib.rs
+
+/Users/sreekareechityala/.cargo/registry/src/index.crates.io-6f17d22bba15001f/simple_asn1-0.4.1/src/lib.rs:
