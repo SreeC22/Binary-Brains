5cd1d9073efe89fc6243267c9f5509426da8fa32
Davor Petrovikj <92484678+dxvx2@users.noreply.github.com>
2024-04-05 02:23:34 -0400

    updated error.rs

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -1,6 +1,7 @@
 use actix_web::{error, HttpResponse, http::StatusCode, ResponseError};
 use derive_more::Display;
 use serde::Serialize;
+use serde_json::json;
 
 #[derive(Debug, Display, Serialize)]
 pub enum ServiceError {
@@ -21,17 +22,23 @@ pub enum ServiceError {
 }
 
 impl ResponseError for ServiceError {
-    fn error_response(&self) -> HttpResponse {
+    fn status_code(&self) -> StatusCode {
         match self {
-            ServiceError::Unauthorized => HttpResponse::Unauthorized().finish(),
-            // Handle other errors
-            _ => HttpResponse::InternalServerError().finish(),
+            ServiceError::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
+            ServiceError::Unauthorized => StatusCode::UNAUTHORIZED,
+            ServiceError::BadRequest(_) => StatusCode::BAD_REQUEST,
+            ServiceError::NotFound => StatusCode::NOT_FOUND,
+            ServiceError::IncorrectPassword => StatusCode::FORBIDDEN,
         }
     }
 
-    // This method constructs the HTTP response to be sent back
-
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code()).json(json!({
+            "error": self.to_string()
+        }))
+    }
 }
+
 impl From<bcrypt::BcryptError> for ServiceError {
     fn from(_: bcrypt::BcryptError) -> Self {
         ServiceError::InternalServerError
