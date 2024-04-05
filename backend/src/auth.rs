a681ba1ae895bd25a8bb3ed73af9487152cadbad
Davor Petrovikj <92484678+dxvx2@users.noreply.github.com>
2024-04-05 02:40:27 -0400

    new gwt

--- backend/src/auth.rs
+++ backend/src/auth.rs
@@ -4,8 +4,11 @@ use std::env;
 use mongodb::{Collection, bson::doc};
 use crate::models::{BlacklistedToken};
 use chrono::{Duration, Utc};
-use jsonwebtoken::{decode, DecodingKey, Validation, errors::Error as JwtError};
+use jsonwebtoken::{decode, DecodingKey, Validation, errors::Error as JwtError, errors::ErrorKind};
 use jsonwebtoken::{encode, Header, EncodingKey};
+use crate::errors::ServiceError; // Ensure you have a ServiceError type defined appropriately
+use log::{warn, info};
+use derive_more::{Display, Error};
 
 use serde::{Serialize, Deserialize};
 
@@ -21,15 +24,36 @@ pub fn verify_password(password: &str, hash: &str) -> Result<bool, bcrypt::Bcryp
 
 
 // Token handling utilities
-pub fn decode_jwt(token: &str) -> Result<Claims, JwtError> {
+
+pub fn decode_jwt(token: &str) -> Result<Claims, ServiceError> {
     let secret_key = env::var("JWT_SECRET_KEY").expect("JWT_SECRET_KEY must be set");
-    decode::<Claims>(
+    
+    match decode::<Claims>(
         token,
         &DecodingKey::from_secret(secret_key.as_bytes()),
         &Validation::default(),
-    )
-    .map(|data| data.claims)
+    ) {
+        Ok(data) => {
+            info!("JWT decoded successfully for email: {}", data.claims.email);
+            Ok(data.claims)
+        },
+        Err(err) => match *err.kind() {
+            ErrorKind::ExpiredSignature => {
+                warn!("Token expired for token: {}", token); // Log the token or a hash of it for security purposes
+                Err(ServiceError::ExpiredToken)
+            },
+            ErrorKind::InvalidToken | ErrorKind::InvalidSignature => {
+                warn!("Invalid token encountered: {}", token); // Consider logging a hash of the token instead
+                Err(ServiceError::InvalidToken)
+            },
+            _ => {
+                warn!("JWT decoding error: {:?}", err);
+                Err(ServiceError::JWTError(format!("{:?}", err)))
+            },
+        },
+    }
 }
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Claims {
     pub email: String,
@@ -44,9 +68,10 @@ pub fn generate_jwt(email: &str, remember_me: bool) -> Result<String, JwtError> 
 
     let claims = Claims {
         email: email.to_owned(),
-        exp: expiration.timestamp(), // Direct assignment without casting
+        exp: expiration.timestamp(),
     };
-    println!("Token expiration timestamp: {}", expiration.timestamp());
+
+    info!("Generating JWT with expiration timestamp: {}", expiration.timestamp());
 
     let secret_key = env::var("JWT_SECRET_KEY").expect("JWT_SECRET_KEY must be set");
     encode(&Header::default(), &claims, &EncodingKey::from_secret(secret_key.as_bytes()))
